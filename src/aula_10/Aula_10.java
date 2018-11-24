219c42b0c2af6c184715318b32e8f9309ef137e3
natan33 <srassis2014@gmail.com>
2018-11-24 11:17:44 -0300

    Quinto commit

--- src/aula_10/Aula_10.java
+++ src/aula_10/Aula_10.java
@@ -5,18 +5,28 @@ import java.util.Scanner;
 
 public class Aula_10 {   
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-         int n1,n2,n3,media;
-         Scanner medias = new Scanner(System.in);
+       
+        Scanner nome =  new Scanner(System.in);
+        Scanner medias = new Scanner(System.in);
+        String qual;
+       
          
-         System.out.println("digite a primeira nota");
+        int n1,n2,n3,media;
+         
+      
+         System.out.print("Informe seu nome :");
+         qual = nome.nextLine();
+         
+         System.out.print("\ndigite a primeira nota :");
          n1 = medias.nextInt();
-         System.out.println("digite a segunda nota");
+         System.out.print("\ndigite a segunda nota :");
          n2 = medias.nextInt();
-         System.out.println("digite a segunda nota");
+         System.out.print("\ndigite a segunda nota :");
          n3 = medias.nextInt();
          media = (n1 + n2 +n3)/3;
-         System.out.println("a media é :" + media);
+         
+         System.out.println("O seu nome: "
+                 +qual+"\nA media é :" + media);
          
         
     }
