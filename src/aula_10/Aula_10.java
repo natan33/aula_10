8d40a5663823734474483cfdcf1fa76b1d00fab8
natan33 <srassis2014@gmail.com>
2018-11-24 10:44:51 -0300

    terceiro commit

--- src/aula_10/Aula_10.java
+++ src/aula_10/Aula_10.java
@@ -1,8 +1,20 @@
 
 package aula_10;
+
+import java.util.Scanner;
+
 public class Aula_10 {   
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         int n1,n2,n3,media;
+         Scanner medias = new Scanner(System.in);
+         
+         System.out.println("digite a primeira nota");
+         n1 = medias.nextInt();
+         System.out.println("digite a segunda nota");
+         n2 = medias.nextInt();
+         
+        
     }
     
 }
